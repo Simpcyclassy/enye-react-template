2bc4ec19127e1489cf952d71bcdf20e4f66632da
Simpcyclassy <34488930+Simpcyclassy@users.noreply.github.com>
2018-08-15 09:35:33 +0100

    Update Interact.js

--- app/components/Interact.js
+++ app/components/Interact.js
@@ -6,16 +6,16 @@ class Interact extends React.Component {
   render() {
     return (
       <div>
-        <div class="header">
-          <div class="container">
+        <div className="header">
+          <div className="container">
             <h1>LETS INTERACT STITCHES</h1>
               <p>Connect With Designers All Over Nigeria</p>
-              <a class="proceed" href="#">Log in</a>
+              <a className="proceed" href="#">Log in</a>
           </div>
         </div>
 
-        <div class="nav">
-          <div class="container">
+        <div className="nav">
+          <div className="container">
             <ul>
               <li><a href="#register">Register</a></li>
               <li><a href="#trainings">Trainings</a></li>
